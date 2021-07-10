3c6cb3e48f30c5deda1720f9be2f8fac3aae1b25
Florian Held <fheldxyz+git@gmail.com>
2021-07-10 10:29:18 +0200

    Initial Commit

---
+++ aare-baer/aare_baerApp.swift
@@ -0,0 +1,17 @@
+//
+//  aare_baerApp.swift
+//  aare-baer
+//
+//  Created by user on 10.07.21.
+//
+
+import SwiftUI
+
+@main
+struct aare_baerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
